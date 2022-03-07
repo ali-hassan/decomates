436048e61aabf8bf5d4c40a09e7eef564ed8993a
Ali Hassan Mirza <ali.hassan.mirzaa+100@gmail.com>
2022-03-07 21:13:55 +0500

    Changes

--- app/controllers/admin2/active_slots_controller.rb
+++ app/controllers/admin2/active_slots_controller.rb
@@ -3,4 +3,14 @@ class Admin2::ActiveSlotsController < ApplicationController
   def index
     @slots = AvailableSlot.where(active: true)
   end
+
+  def new
+    @category = AvailableSlot.new
+    @shapes = @current_community.shapes
+    @selected_shape_ids = @shapes.map { |s| s[:id] }
+    render layout: false
+  end
+  def show
+
+  end
 end
