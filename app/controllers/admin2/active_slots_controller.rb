05d1758b59c8c3c2fc6f7b27de905ff85a64a06b
Ali Hassan Mirza <ali.hassan.mirzaa+100@gmail.com>
2022-03-09 18:16:48 +0500

    Applied changes and made the flow

--- app/controllers/admin2/active_slots_controller.rb
+++ app/controllers/admin2/active_slots_controller.rb
@@ -1,8 +1,8 @@
-class Admin2::ActiveSlotsController < ApplicationController
-  # before_action :find_category, except: %i[index order change_category]
+class Admin2::ActiveSlotsController < Admin2::AdminBaseController
+  before_action :find_category, only: %i[edit update destroy]
 
   def index
-    @slots = AvailableSlot.where(active: true)
+    @slots = AvailableSlot.all
   end
 
   def new
@@ -11,11 +11,36 @@ class Admin2::ActiveSlotsController < ApplicationController
     @selected_shape_ids = @shapes.map { |s| s[:id] }
     render layout: false
   end
+
+  def edit
+    @shapes = @current_community.shapes
+    @selected_shape_ids = CategoryListingShape.where(category_id: @category.id).map(&:listing_shape_id)
+    render layout: false
+  end
+
+  def update
+    @category.update!(category_params)
+  rescue StandardError=> e
+    flash[:error] = e.message
+  ensure
+    redirect_to admin2_listings_categories_path
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to admin2_listings_categories_path
+  end
+
   def create
-    @slot = AvailableSlot.create name: params[:name], day: params[:day], active: params[:available_slot][:active]
+    @slot = AvailableSlot.create name: params[:name], day: params[:day], active: (params[:available_slot].present? ? params[:available_slot][:active] : false)
     redirect_to "/en/admin"
   end
 
   def show
+  end
+
+  private
+  def find_category
+    @category = AvailableSlot.find_by_id(params[:id])
   end
 end
